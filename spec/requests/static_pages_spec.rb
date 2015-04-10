72346a225558398c41046efa60038c743f5a0035
SamuraiT <t.yasukawa01@gmail.com>
2015-04-10 18:01:04 +0900

    add soln. for exer3.5.2 #3

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 describe "Static Pages" do
+
+  let(:base_title){"SampleApp|"}
+
   describe "Home Page"  do
     it "should have the content 'Sample App'" do |variable|
       visit '/static_pages/home'
@@ -9,7 +12,7 @@ describe "Static Pages" do
 
     it "should have the title 'Home'" do |variable|
       visit '/static_pages/home'
-      expect(page).to have_title("Home")
+      expect(page).to have_title("#{base_title} Home")
     end
   end
 
@@ -22,7 +25,7 @@ describe "Static Pages" do
 
     it "should have the title 'Help'" do
      visit '/static_pages/help'
-     expect(page).to have_title('Help')
+     expect(page).to have_title("#{base_title} Help")
     end
   end
 
@@ -34,7 +37,7 @@ describe "Static Pages" do
 
     it "should have the content 'About us'" do
       visit '/static_pages/about'
-      expect(page).to have_title('About Us')
+      expect(page).to have_title("#{base_title} About Us")
     end
   end
 
@@ -44,6 +47,12 @@ describe "Static Pages" do
       i|
       visit '/static_pages/contact'
       expect(page).to have_content('Contact')
+    end
+
+    it "should have the title 'Contact'" do |
+      i|
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title} Contact")
     end
   end
 end
